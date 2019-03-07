18409763079da75b15de54bbef5880432c3e3b69
Sam Bengtson <sambeng1@gmail.com>
2019-03-07 10:52:16 -0600

    Added stats file, added button

--- App.js
+++ App.js
@@ -7,26 +7,26 @@
  * @lint-ignore-every XPLATJSCOPYRIGHT1
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from 'react'
+import { Platform, StyleSheet, Text, View } from 'react-native'
+import GetGameButton from './components/GetGamesButton'
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android:
     'Double tap R on your keyboard to reload,\n' +
     'Shake or press menu button for dev menu',
-});
+})
 
-type Props = {};
+type Props = {}
 export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Text style={styles.welcome}>WELCOME TO THE MEATSHOW</Text>
+        <GetGameButton />
       </View>
-    );
+    )
   }
 }
 
@@ -47,4 +47,4 @@ const styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
-});
+})
---
+++ components/GetGamesButton.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Button } from 'react-native'
+
+export const GetGameButton = () => {
+  return <Button title="Show Me The Games" />
+}
+
+export default GetGameButton
